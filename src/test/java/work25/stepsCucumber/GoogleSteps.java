56c44ce4a093ff130bcdb6d14901c98849bc80c2
llARXANGELll <aramh@inbox.ru>
2020-04-01 00:43:34 +0300

    Все работаеt v3

--- src/test/java/work25/stepsCucumber/GoogleSteps.java
+++ src/test/java/work25/stepsCucumber/GoogleSteps.java
@@ -10,35 +10,4 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class GoogleSteps {
 
-
-    @Дано("пользователь входит на сайт")
-    public void пользователь_входит_на_сайт() {
-        open("https://idemo.bspb.ru/");
-        LoginPage login = new LoginPage();
-        login.verificationTitile("Интернет банк - Банк Санкт-Петербург")
-                .loginInput("demo")
-                .passwordInput("demo")
-                .loginButton();
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Дано("вводит логин и пароль")
-    public void вводит_логин_и_пароль() {
-        SmsConfirmationPage smsConfirmationPage = new SmsConfirmationPage();
-        smsConfirmationPage.otpCode("0000")
-                .inputButton();
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Когда("бла бла бла")
-    public void бла_бла_бла() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Тогда("ололо")
-    public void ололо() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
 }
