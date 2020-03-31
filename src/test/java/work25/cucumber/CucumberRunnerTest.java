a5ec0319b5de716df5b1983432e8b47c52d233dc
llARXANGELll <aramh@inbox.ru>
2020-04-01 01:07:45 +0300

    Все работаеt v6

--- src/test/java/work25/cucumber/CucumberRunnerTest.java
+++ src/test/java/work25/cucumber/CucumberRunnerTest.java
@@ -14,40 +14,8 @@ import static com.codeborne.selenide.Selenide.open;
 @CucumberOptions(
         plugin = "json:target/cucumber-report.json",
         features = "src/test/resources/feature",
-        glue = "work25.cucumber"
+        glue = "work25.stepsCucumber"
 )
 public class CucumberRunnerTest extends AbstractTestNGCucumberTests {
-    @Когда("пользователь входит на сайт")
-    public void пользователь_входит_на_сайт() {
-        open("https://idemo.bspb.ru/");
-        LoginPage login = new LoginPage();
-        login.verificationTitile("Интернет банк - Банк Санкт-Петербург")
-                .loginInput("demo")
-                .passwordInput("demo")
-                .loginButton();
-    }
 
-    @Затем("вводит логин")
-    public void вводит_логин() {
-        SmsConfirmationPage smsConfirmationPage = new SmsConfirmationPage();
-        smsConfirmationPage.otpCode("0000")
-                .inputButton();
-
-    }
-
-    @Затем("вводит парль")
-    public void вводит_парль() {
-        System.out.println("1");
-    }
-
-    @Затем("нажимает кнопку войти")
-    public void нажимает_кнопку_войти() {
-
-        System.out.println("2");
-    }
-
-    @Затем("открывается страница подтверждения")
-    public void открывается_страница_подтверждения() {
-        System.out.println("3");
-    }
 }
--- src/test/java/work25/stepsCucumber/GoogleSteps.java
+++ src/test/java/work25/stepsCucumber/GoogleSteps.java
@@ -1,6 +1,7 @@
 package work25.stepsCucumber;
 
 import io.cucumber.java.ru.Дано;
+import io.cucumber.java.ru.Затем;
 import io.cucumber.java.ru.Когда;
 import io.cucumber.java.ru.Тогда;
 import work23.LoginPage;
@@ -9,5 +10,38 @@ import work23.SmsConfirmationPage;
 import static com.codeborne.selenide.Selenide.open;
 
 public class GoogleSteps {
+    @Когда("пользователь входит на сайт")
+    public void пользователь_входит_на_сайт() {
+        open("https://idemo.bspb.ru/");
+        LoginPage login = new LoginPage();
+        login.verificationTitile("Интернет банк - Банк Санкт-Петербург")
+                .loginInput("demo")
+                .passwordInput("demo")
+                .loginButton();
+    }
+
+    @Затем("вводит логин")
+    public void вводит_логин() {
+        SmsConfirmationPage smsConfirmationPage = new SmsConfirmationPage();
+        smsConfirmationPage.otpCode("0000")
+                .inputButton();
+
+    }
+
+    @Затем("вводит парль")
+    public void вводит_парль() {
+        System.out.println("1");
+    }
+
+    @Затем("нажимает кнопку войти")
+    public void нажимает_кнопку_войти() {
+
+        System.out.println("2");
+    }
+
+    @Затем("открывается страница подтверждения")
+    public void открывается_страница_подтверждения() {
+        System.out.println("3");
+    }
 
 }
