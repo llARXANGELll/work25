a1deef68730bfb926bfde4e71a46aa609495d9cb
llARXANGELll <aramh@inbox.ru>
2020-04-04 03:06:17 +0300

    Не могу понять почему падает

--- src/test/java/work25/cucumber/CucumberRunnerTest.java
+++ src/test/java/work25/cucumber/CucumberRunnerTest.java
@@ -1,15 +1,9 @@
 package work25.cucumber;
 
-import com.codeborne.selenide.Config;
 import com.codeborne.selenide.Configuration;
-import io.cucumber.java.ru.Затем;
-import io.cucumber.java.ru.Когда;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.BeforeMethod;
-import work23.LoginPage;
-import work23.SmsConfirmationPage;
-import work25.Work25Test;
 
 import static com.codeborne.selenide.Selenide.open;
 
