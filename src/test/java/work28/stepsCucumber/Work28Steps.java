e4294a772e858f237b73e130a55cbd2935c2e3cd
llARXANGELll <aramh@inbox.ru>
2020-04-10 00:56:44 +0300

    Настроил 28

---
+++ src/test/java/work28/stepsCucumber/Work28Steps.java
@@ -0,0 +1,18 @@
+package work28.stepsCucumber;
+
+import io.cucumber.java.ru.Допустим;
+import org.testng.Assert;
+import work27.DepositManagePage;
+import work27.DepositPage;
+import work27.HomePage;
+
+import java.util.List;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.switchTo;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+
+public class Work28Steps {
+
+
+}
