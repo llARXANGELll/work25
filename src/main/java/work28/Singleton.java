2675518a3cd51624efc7f2c116dce175b1aad5f1
llARXANGELll <aramh@inbox.ru>
2020-04-15 15:51:50 +0300

    Настраиваю ворк29 v2

--- src/main/java/work28/Singleton.java
+++ src/main/java/work28/Singleton.java
@@ -1,20 +1,20 @@
-package work28;
-
-import com.codeborne.selenide.SelenideElement;
-
-public final class Singleton {
-    private static SelenideElement sumValueAutoAllOffers;
-
-
-    public static SelenideElement getSumValueAutoAllOffers() {
-        if (sumValueAutoAllOffers == null) {
-            sumValueAutoAllOffers = null;
-        }
-        return sumValueAutoAllOffers;
-    }
-
-    public void setSumValueAutoAllOffers(SelenideElement sumValueAutoAllOffers) {
-        this.sumValueAutoAllOffers = sumValueAutoAllOffers;
-    }
-
-}
+//package work28;
+//
+//import com.codeborne.selenide.SelenideElement;
+//
+//public final class Singleton {
+//    private static SelenideElement sumValueAutoAllOffers;
+//
+//
+//    public static SelenideElement getSumValueAutoAllOffers() {
+//        if (sumValueAutoAllOffers == null) {
+//            sumValueAutoAllOffers = null;
+//        }
+//        return sumValueAutoAllOffers;
+//    }
+//
+//    public void setSumValueAutoAllOffers(SelenideElement sumValueAutoAllOffers) {
+//        this.sumValueAutoAllOffers = sumValueAutoAllOffers;
+//    }
+//
+//}
