4f37d322997913d79d26cf7b084df946ceb5eeb5
llARXANGELll <aramh@inbox.ru>
2020-04-15 17:02:13 +0300

    Сингл

--- src/main/java/work28/SavingValueAvtoRu.java
+++ src/main/java/work28/SavingValueAvtoRu.java
@@ -1,14 +1,8 @@
 package work28;
 
 public class SavingValueAvtoRu {
-    private static String sumValueAutoAllOffers;
 
-    private static SavingValueAvtoRu getInstance() {
-        if (sumValueAutoAllOffers == null) {
-            sumValueAutoAllOffers = null;
-        }
-        return getInstance();
-    }
+    private static String sumValueAutoAllOffers;
 
     public static String getSumValueAutoAllOffers() {
         return sumValueAutoAllOffers;
