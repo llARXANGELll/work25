aedf13d2f34e054cdb085ce5896091d10e0297c8
llARXANGELll <aramh@inbox.ru>
2020-04-18 22:10:09 +0300

    настраиваю

--- src/test/java/work29/cucumber/CucumberRunnerTest.java
+++ src/test/java/work29/cucumber/CucumberRunnerTest.java
@@ -1,9 +1,14 @@
 package work29.cucumber;
 
 import com.codeborne.selenide.Configuration;
+import io.cucumber.java.Before;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.BeforeMethod;
+
+import javax.rmi.CORBA.Util;
+import java.io.*;
+import java.util.Properties;
 
 @CucumberOptions(
         plugin = "json:target/cucumber-report.json",
@@ -11,16 +16,23 @@ import org.testng.annotations.BeforeMethod;
         snippets = CucumberOptions.SnippetType.CAMELCASE,
         glue ={
                 "work28.stepsCucumber",
-                "work27.stepsCucumber"
+                "work27.stepsCucumber",
+                "work30.hooks"
         },
         tags = "@work28 or @work27"
 )
 
 public class CucumberRunnerTest extends AbstractTestNGCucumberTests {
-
-    @BeforeMethod
-    public void selenideConfiguration() {
-        Configuration.browser = "chrome";
-        Configuration.clickViaJs = true;
-    }
+        @Before
+        public void selenideConfiguration() throws FileNotFoundException {
+                FileInputStream fils;
+                Properties properties = new Properties();
+                try {
+                        fils = new FileInputStream("src/test/resources/config/cucumberConfig.properties");
+                        properties.load(fils);
+                } catch (IOException ignored) {
+                }
+                Configuration.browser = properties.getProperty("browser");
+                Configuration.clickViaJs = true;
+        }
 }
